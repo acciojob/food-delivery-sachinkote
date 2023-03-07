2652a75dbbad3ed15b19fb6c39e74c6881407852
sachinkote <65725161+sachinkote@users.noreply.github.com>
2023-03-07 13:18:28 +0530

    Update FoodService.java

--- src/main/java/com/driver/service/FoodService.java
+++ src/main/java/com/driver/service/FoodService.java
@@ -1,17 +1,88 @@
-package com.driver.service;
+package com.driver.service.impl;
 
+import com.driver.io.entity.FoodEntity;
+import com.driver.io.repository.FoodRepository;
+import com.driver.service.FoodService;
+import com.driver.shared.dto.FoodDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import com.driver.shared.dto.FoodDto;
+public class FoodServiceImpl implements FoodService {
 
-/**
- * Handle exception cases for all methods which throw Exception
- */
-public interface FoodService {
+    @Autowired
+    FoodRepository foodRepository;
 
-	FoodDto createFood(FoodDto food);
-	FoodDto getFoodById(String foodId) throws Exception;
-	FoodDto updateFoodDetails(String foodId, FoodDto foodDetails) throws Exception;
-	void deleteFoodItem(String id) throws Exception;
-	List<FoodDto> getFoods();
+    @Override
+    public FoodDto createFood(FoodDto food) {
+
+        FoodEntity foodEntity = new FoodEntity();
+        foodEntity.setFoodCategory(food.getFoodCategory());
+        foodEntity.setFoodId(food.getFoodId());
+        foodEntity.setFoodName(food.getFoodName());
+        foodEntity.setFoodPrice(food.getFoodPrice());
+
+        foodRepository.save(foodEntity);
+
+        return food;
+    }
+
+    @Override
+    public FoodDto getFoodById(String foodId) throws Exception {
+
+        FoodEntity foodEntity = foodRepository.findByFoodId(foodId);
+
+        FoodDto foodDto = new FoodDto();
+        foodDto.setFoodPrice(foodEntity.getFoodPrice());
+        foodDto.setFoodName(foodEntity.getFoodName());
+        foodDto.setFoodCategory(foodEntity.getFoodCategory());
+        foodDto.setFoodId(foodEntity.getFoodId());
+        return foodDto;
+    }
+
+    @Override
+    public FoodDto updateFoodDetails(String foodId, FoodDto foodDetails) throws Exception {
+
+        FoodEntity foodEntity = foodRepository.findByFoodId(foodId);
+        foodEntity.setFoodPrice(foodDetails.getFoodPrice());
+        foodEntity.setFoodName(foodDetails.getFoodName());
+        foodEntity.setFoodCategory(foodDetails.getFoodCategory());
+        foodEntity.setFoodId(foodDetails.getFoodId());
+
+        foodRepository.save(foodEntity);
+
+        return foodDetails;
+    }
+
+    @Override
+    public void deleteFoodItem(String id) throws Exception {
+
+        FoodEntity foodEntity = foodRepository.findByFoodId(id);
+        long Id = foodEntity.getId();
+        foodRepository.deleteById(Id);
+
+
+    }
+
+    @Override
+    public List<FoodDto> getFoods() {
+
+        List<FoodEntity>foodEntityList = (List<FoodEntity>) foodRepository.findAll();
+
+        List<FoodDto>foodDtoList = new ArrayList<>();
+
+        for(FoodEntity foodEntity:foodEntityList){
+            FoodDto foodDto = new FoodDto();
+            foodDto.setFoodId(foodEntity.getFoodId());
+            foodDto.setFoodName(foodEntity.getFoodName());
+            foodDto.setFoodPrice(foodEntity.getFoodPrice());
+            foodDto.setFoodCategory(foodEntity.getFoodCategory());
+
+            foodDtoList.add(foodDto);
+        }
+
+
+        return foodDtoList;
+    }
 }
